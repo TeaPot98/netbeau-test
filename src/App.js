7241982f8672c9e06ad64365e5b1b646a9552b03
TeaPot98 <trifautan@gmail.com>
2022-09-06 20:02:33 +0300

    added url search query live update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import data from './data'
 
 const App = () => {
   const [searchString, setSearchString] = useState('')
   const [dataToShow, setDataToShow] = useState(data)
+  const url = new URL(window.location)
+  const params = new URLSearchParams(url.search)
   
-  const handleSearch = (event) => {
-    setSearchString(event.target.value)
+  const handleSearch = (string) => {
+    setSearchString(string)
+    // Filter dataset
     setDataToShow(
-      data.filter(item => item.toLowerCase().includes(event.target.value.toLowerCase()))
+      data.filter(item => item.toLowerCase().includes(string.toLowerCase()))
     )
-    // console.log(event.target.value)
+    // building new url
+    const newUrl = window.location.origin + window.location.pathname + (string!== '' ? `?search=${string}` : '')
+    
+    // updating the url w/o refresh
+    window.history.pushState({path: newUrl}, '', newUrl)
   }
+
+  useEffect(() => {
+    if (params.has('search')) {
+      const querySearch = params.get('search')
+      handleSearch(querySearch)
+    }
+  }, [])
+  
   
   return (
     <div className="App">
-      <input value={searchString} onChange={handleSearch} type="search" placeholder="Search..." />
+      <input value={searchString} onChange={(event) => handleSearch(event.target.value)} type="search" placeholder="Search..." />
       <ul>
         {dataToShow.map(d =>
           <li key={d}>{d}</li>
